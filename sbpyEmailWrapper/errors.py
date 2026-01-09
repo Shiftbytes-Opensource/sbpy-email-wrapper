91ced839b2ca9c75cb94938bfa4c6e84a056afda
sreenath <sreenath@shiftbytes.co.in>
2026-01-10 01:02:49 +0530

    added library files

---
+++ sbpyEmailWrapper/errors.py
@@ -0,0 +1,17 @@
+class InvalidToEmailAddress(Exception):
+    pass
+
+class InvalidFromEmailAddress(Exception):
+    pass
+
+class InvlidEmailService(Exception):
+    pass
+
+class AttachmentsNotsupported(Exception):
+    pass
+
+class AsyncSendNotSupported(Exception):
+    pass
+
+class NoCallbackFunctionGiven(Exception):
+    pass
