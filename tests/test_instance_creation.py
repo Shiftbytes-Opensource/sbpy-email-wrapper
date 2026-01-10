bb3df32f4189906d657bd825e36942247f227fed
Sreenath <sreenath@shiftbytes.co.in>
2026-01-10 23:38:42 +0530

    updated tests

--- tests/test_instance_creation.py
+++ tests/test_instance_creation.py
@@ -2,7 +2,7 @@ import pytest
 from sbpyEmailWrapper.wrapper import EmailWrapper
 
 def test_instantiation():
-    inst = EmailWrapper()
+    inst = EmailWrapper("aws_ses")
     assert isinstance(inst, EmailWrapper) == True
 
 if __name__ == "__main__":
